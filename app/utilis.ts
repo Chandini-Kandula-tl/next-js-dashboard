c9316523d71829fa3e9d8cdf1f983abcbc1ec9bf
Chandini-Kandula-tl <chandini@tilicho.in>
2024-10-28 16:44:26 +0530

    next-js-dashboard project initial commit

---
+++ app/utilis.ts
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+interface DataProps {
+  id: number;
+  name: string;
+  age: number;
+}
+const useFetchUsersData = (rawData?: DataProps[]) => {
+  const [data, setData] = useState<DataProps[]>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+
+        const mockData = [
+          { id: 1, name: "John Doe", age: 30 },
+          { id: 2, name: "Jane Smith", age: 25 },
+        ];
+
+        setData(rawData ?? mockData);
+      } catch (err) {
+        setError("An error occurred while fetching data.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useFetchUsersData;
